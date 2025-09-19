b99fc4a12867e535305390e21f638c37b1464f78
Rainer Stropek <rainer@timecockpit.com>
2025-09-19 10:44:18 +0200

    Saved in TypeScript Playground at 2025-09-19T08:44:18.181Z

--- cat/index.ts
+++ cat/index.ts
@@ -1,14 +1,25 @@
 function setup() {
-    createCanvas(800,800)
-    background("skyblue")
+  createCanvas(400, 400);
+  background("aqua");
 
-    circle(400,400,600)
+  fill("lightgray");
+  ellipse(200, 250, 100, 100);
 
-    fill("black")
-    circle(500,375,100)
+  triangle(160, 210, 180, 200, 170, 180);
+  triangle(240, 210, 220, 200, 230, 180);
 
-    fill("black")
-    circle(300,375,100)
+  fill("black");
+  ellipse(185, 245, 10, 10)
+  ellipse(215, 245, 10, 10)
 
-    
+  fill("pink");
+  triangle(195, 255, 205, 255, 200, 260)
+
+  line(170, 255, 190, 260)
+  line(170, 265, 190, 265)
+  line(170, 270, 190, 275)
+  line(230, 255, 210, 260)
+  line(230, 265, 210, 265)
+  line(230, 270, 210, 275)
+triangle(                                  )
 }
