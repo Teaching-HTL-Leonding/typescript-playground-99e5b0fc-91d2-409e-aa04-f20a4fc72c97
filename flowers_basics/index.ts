cf910fb0ddee9ea07f2cd179f635622aa4d6e0c8
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:14:57 +0200

    Saved in TypeScript Playground at 2025-10-10T07:14:56.904Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -2,6 +2,7 @@
 
 function setup() {
 createCanvas(1000,1000)
+//background("skyblue")
 
 stroke("lightgreen")
 strokeWeight(10)
@@ -9,20 +10,29 @@ strokeWeight(10)
 //circle von Blättern
 fill("lightgreen")
 
-circle(60,70,63.8)
+circle(55,70,70)
 
-circle(115,55,63.8)
+circle(105,50,70)
 
-circle(160,90,63.8)
+circle(150,90,70)
 
-circle(120,140,63.8)
+circle(100+15.5,137.5,70)
 
-circle(60,125,63.8)
+circle(60,90+31.9,70)
 
 //KERN
 fill("yellow")
 
-circle(100,90,63.8)
+circle(100,90,65)
+
+//STÄNGEL
+fill("darkgreen")
+stroke("darkgreen")
+
+//angleMode(DEGRES)
+strokeWeight(10)
+noFill()
+arc(60,130,200,100,-15,15)
 
 
 }
