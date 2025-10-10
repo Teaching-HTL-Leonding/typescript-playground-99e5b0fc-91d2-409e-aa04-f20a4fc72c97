9ac51ceb0f5016d4183e9db6aeb0ef8000fe3c0d
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:49:04 +0200

    Saved in TypeScript Playground at 2025-10-10T07:49:03.800Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -1,9 +1,9 @@
-const STROKETHICK=10
-const STROKETHIN=10
+const STROKETHICK = 10
+const STROKETHIN = 10
 
-  function setup() {
-    createCanvas(500,300)
- background("skyblue")
+function setup() {
+  createCanvas(500, 300)
+  background("skyblue")
 
   //STÄNGEL
   fill("darkgreen")
@@ -12,7 +12,7 @@ const STROKETHIN=10
   angleMode(DEGREES)
   strokeWeight(15)
   noFill()
-  arc(60,190,220,220,-31,45)
+  arc(60, 190, 220, 220, -31, 45)
 
 
   stroke("lightgreen")
@@ -21,20 +21,20 @@ const STROKETHIN=10
   //circle von Blättern
   fill("lightgreen")
 
-  circle(55,70,70)
+  circle(55, 70, 70)
 
-  circle(105,50,70)
+  circle(105, 50, 70)
 
-  circle(150,90,70)
+  circle(150, 90, 70)
 
-  circle(100+15.5,137.5,70)
+  circle(100 + 15.5, 137.5, 70)
 
-  circle(60,90+31.9,70)
+  circle(60, 90 + 31.9, 70)
 
   //KERN
   fill("yellow")
 
-  circle(100,90,65)
+  circle(100, 90, 65)
 
   //2. Blume
 
@@ -45,21 +45,21 @@ const STROKETHIN=10
   angleMode(DEGREES)
   strokeWeight(15)
   noFill()
-  arc(260,190,220,220,-31,45)
+  arc(260, 190, 220, 220, -31, 45)
 
   //circle von Blättern
   strokeWeight(2.5)
   stroke("black")
   fill("lightgreen")
 
-  circle(320,60,80)
-  circle(320,160,80)
+  circle(320, 60, 80)
+  circle(320, 160, 80)
 
-  circle(270,110,80)
-  circle(370,110,80)
+  circle(270, 110, 80)
+  circle(370, 110, 80)
 
   //KERN
   fill("yellow")
-  circle(320,110,80)
+  circle(320, 110, 80)
 
-  }
+}
