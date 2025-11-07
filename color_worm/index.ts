06e8077188e79b413f1554a032b1177e035a713d
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:28:47 +0100

    Saved in TypeScript Playground at 2025-11-07T07:28:46.757Z

---
+++ color_worm/index.ts
@@ -0,0 +1,6 @@
+function setup() {
+}
+
+function mouseMoved() {
+}
+https://ts-web-playground-server-bfdfbjcthubcardq.westeurope-01.azurewebsites.net/github/exercise/image-proxy?imageUrl=https%3A%2F%2Fgithub.com%2Frstropek%2Fts-web-playground%2Fblob%2Fmain%2Fexercises%2F0020-Variables%2Fcolor_worm.png%3Fraw%3Dtrue$0
