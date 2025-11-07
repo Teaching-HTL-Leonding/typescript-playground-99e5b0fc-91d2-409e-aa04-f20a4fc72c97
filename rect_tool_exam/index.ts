f08660bb192e49b57e17551562ad0eb97d403f8a
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:43:25 +0100

    Saved in TypeScript Playground at 2025-11-07T07:43:24.572Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -1,20 +1,24 @@
-function setup() {
-    createCanvas(400, 200);
-    background("black");
-    angleMode(DEGREES);
+    function setup() {
+        createCanvas(400, 200);
+        background("black");
+        angleMode(DEGREES);
+    }
+    let color_hue: number = 0
+
+    function mouseMoved() {
+        const gridSize = 20;
+
+        background("black");
+
+        strokeWeight(2);
+        noFill();
+
+    fill(color_hue,100,100)
+    rect(mouseX,mouseY,200)
+
+    //color_hue = (color_hue + 5) %360
+        textAlign(LEFT);
+    const area = Math.round(diameterX / 2 * diameterY / 2 * Math.PI);
+    text(`Area: ${area}`, 5, height - 10);
+}      
 }
-let color_hue: number = 0
-
-function mouseMoved() {
-    const gridSize = 20;
-
-    background("black");
-
-    strokeWeight(2);
-    noFill();
-
-fill(color_hue,100,100)
-rect(mouseX,mouseY,40)
-
-color_hue = (color_hue + 5) 
-   
