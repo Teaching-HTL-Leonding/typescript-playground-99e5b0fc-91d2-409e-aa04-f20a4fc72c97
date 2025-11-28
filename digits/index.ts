3fb484b01bdeb0a1e36a562bc3bc1568fd73c67d
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 08:58:33 +0100

    Saved in TypeScript Playground at 2025-11-28T07:58:33.231Z

---
+++ digits/index.ts
@@ -0,0 +1,44 @@
+function setup() {
+  const num = Math.floor(random(0, 1_000_000));
+
+// Add the necessary code here
+let numString = num.toString();
+
+const boxWidth = 50;
+const boxHeight = 70;
+const spacing = 10;
+const totalWidth = numString.length * boxWidth + (numString.length - 1) * spacing;
+
+const startX = (width - totalWidth) / 2;
+const startY = height / 2 - 50;
+
+stroke(255, 204, 0);
+strokeWeight(2);
+fill(0);
+rectMode(CORNER);
+
+textSize(32);
+fill(255, 204, 0);
+textAlign(CENTER, CENTER);
+
+for (let i = 0; i < numString.length; i++) {
+  const digit = numString[i];
+  const x = startX + i * (boxWidth + spacing);
+  const y = startY;
+
+  rect(x, y, boxWidth, boxHeight);
+
+  text(digit, x + boxWidth / 2, y + boxHeight / 2);
+}
+
+fill(255);
+textSize(18);
+textAlign(CENTER, TOP);
+noStroke();
+text(num, width / 2, height - 20);
+  
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  text(num, width / 2, height - 20);
+}
