ea07424f8b7c4750c6b74123d1ff3eaa38a24533
Rainer Stropek <rainer@timecockpit.com>
2025-12-04 12:59:44 +0100

    Saved in TypeScript Playground at 2025-12-04T11:59:44.098Z

--- bouncing_ball/index.ts
+++ bouncing_ball/index.ts
@@ -1,28 +1,38 @@
 function setup() {
-  createCanvas(300, 200);
+    createCanvas(300, 200);
 }
 
 const circleDiameter = 50;
 
 let circleCenterX = 0;
-let direction = 2;
+let circleCenterY = 0;
+let directionHorizontal = 2;
+let directionVertical = 2;
 
 // Remember: The _draw_ method is called FOR EVERY FRAME
 function draw() {
-  background("gold");
+    background("gold");
 
-  stroke("white");
-  strokeWeight(3);
-  fill("lime");
-  circle(circleCenterX, height / 2, circleDiameter);
+    stroke("white");
+    strokeWeight(3);
+    fill("lime");
+    circle(circleCenterX, height / 2, circleDiameter);
 
-  circleCenterX += direction;
+    fill("aqua");
+    circle(width / 2, circleCenterY, circleDiameter);
 
-  //                         +----------------------------- OR operator
-  //                         |
-  //                         v
-  if (circleCenterX >= width || circleCenterX <= 0) {
-    // Reverse sign of direction (positive -> right, negative -> left)
-    direction *= -1; 
-  }
+    circleCenterX += directionHorizontal;
+    circleCenterY += directionVertical;
+
+    //                         +----------------------------- OR operator
+    //                         |
+    //                         v
+    if (circleCenterX >= width || circleCenterX <= 0) {
+        // Reverse sign of direction (positive -> right, negative -> left)
+        directionHorizontal *= -1; 
+    }
+
+    if (circleCenterY >= height || circleCenterY <= 0) {
+        directionVertical *= -1; 
+    }
 }
