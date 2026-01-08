3dc25966e9e2956a240d7cc093fd11ea0e33d0a2
Rainer Stropek <rainer@timecockpit.com>
2026-01-08 13:09:20 +0100

    Saved in TypeScript Playground at 2026-01-08T12:09:20.491Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,5 +1,15 @@
 function setup() {
+     const centerX = [100
+    const centerY = 100
+
+    createCanvas(500,500)
+    background("white")
+
+
+    fill("red")
+    y
 }
 
 function draw() {
+  
 }
