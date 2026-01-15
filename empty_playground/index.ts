3ccc756feab8b6001a68797860c47af3ac29a5a2
Rainer Stropek <rainer@timecockpit.com>
2026-01-15 13:30:32 +0100

    Saved in TypeScript Playground at 2026-01-15T12:30:31.658Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,21 +1,32 @@
+let rotation: number[] = []
+let centerX: number[] = []
+let centerY: number[] = []
+ 
 function setup() {
-
-createCanvas(500,500)
-stroke("black")
-strokeWeight(2)
-noFill()
-angleMode(DEGREES)
-
-
+    createCanvas(1000, 1000)
+    stroke("black")
+    strokeWeight(2)
+    noFill()
+    angleMode(DEGREES)
 }
-
+ 
+ 
 function draw() {
-    background("white")
-    push()
-    rotate   
-
+    background("red")
+ 
+    for (let i = 0; i < centerX.length; i++) {
+        push()
+        translate(centerX[i], centerY[i])
+        rotate(rotation[i])
+        line(-50, 0, 50, 0)
+        pop()
+        rotation[i] += 5
+    }
+ 
 }
-
-function mouseClicked()  {
-
+ 
+function mouseClicked() {
+    centerX.push (mouseX)
+    centerY.push (mouseY)
+    rotation.push(0)
 }
